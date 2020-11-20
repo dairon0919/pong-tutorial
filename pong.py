3bbbe4425315c55c00b4b1f551e34364e010dddb
dairon0919 <daironrocks@gmail.com>
2020-11-20 18:31:56 -0500

    polished finished code

--- pong.py
+++ pong.py
@@ -120,15 +120,15 @@ while True:
     # Paddle ball collision 
     if (hit_ball.xcor() > 360 and
                         hit_ball.xcor() < 370 and
-                        (hit_ball.ycor() < right_pad.ycor()+40) and
-                        hit_ball.ycor() > right_pad.ycor()-40): 
+                        (hit_ball.ycor() < right_pad.ycor()+70) and
+                        hit_ball.ycor() > right_pad.ycor()-70): 
         hit_ball.setx(360) 
         hit_ball.dx*=-1
          
     if (hit_ball.xcor()<-360 and 
                        hit_ball.xcor()>-370 and 
-                       (hit_ball.ycor()<left_pad.ycor()+40) and 
-                        hit_ball.ycor()>left_pad.ycor()-40): 
+                       (hit_ball.ycor()<left_pad.ycor()+70) and 
+                        hit_ball.ycor()>left_pad.ycor()-70): 
         hit_ball.setx(-360) 
         hit_ball.dx*=-1
 turtle.done()
