9f2d57c997a23755376c1d11569b2f7a10774fe1
dairon0919 <daironrocks@gmail.com>
2020-11-20 01:58:26 -0500

    finished project

--- pong.py
+++ pong.py
@@ -1,20 +1,20 @@
-#pong tutorial by free code camp
-
+#pong tutorial by geeks for geeks
+"see https://www.geeksforgeeks.org/create-pong-game-using-python-turtle/"
 
 
 import turtle
 import tkinter
 
 sc = turtle.Screen()
-sc.title("pong game")
-sc.bgcolor("white")
+sc.title("giants pong game")
+sc.bgcolor("blue")
 sc.setup(width=1000, height=600)
 
 # Left Paddle
 left_pad=turtle.Turtle()
 left_pad.speed(0)
 left_pad.shape("square")
-left_pad.color("black")
+left_pad.color("red")
 left_pad.shapesize(stretch_wid=6, stretch_len=2)
 left_pad.penup()
 left_pad.goto(-400,0)
@@ -23,7 +23,7 @@ left_pad.goto(-400,0)
 right_pad = turtle.Turtle() 
 right_pad.speed(0) 
 right_pad.shape("square") 
-right_pad.color("black") 
+right_pad.color("red") 
 right_pad.shapesize(stretch_wid=6, stretch_len=2) 
 right_pad.penup() 
 right_pad.goto(400, 0) 
@@ -32,7 +32,7 @@ right_pad.goto(400, 0)
 hit_ball = turtle.Turtle() 
 hit_ball.speed(40) 
 hit_ball.shape("circle") 
-hit_ball.color("blue") 
+hit_ball.color("red") 
 hit_ball.penup() 
 hit_ball.goto(0, 0) 
 hit_ball.dx = 5
@@ -46,7 +46,7 @@ right_player = 0
 # Displays the score 
 sketch = turtle.Turtle() 
 sketch.speed(0) 
-sketch.color("blue") 
+sketch.color("white") 
 sketch.penup() 
 sketch.hideturtle() 
 sketch.goto(0, 260) 
@@ -119,15 +119,16 @@ while True:
   
     # Paddle ball collision 
     if (hit_ball.xcor() > 360 and
-        hit_ball.xcor() < 370 and
-        (hit_ball.ycor() < right_pad.ycor()+40 and
-        hit_ball.ycor() > right_pad.ycor()-40): 
+                        hit_ball.xcor() < 370 and
+                        (hit_ball.ycor() < right_pad.ycor()+40) and
+                        hit_ball.ycor() > right_pad.ycor()-40): 
         hit_ball.setx(360) 
         hit_ball.dx*=-1
          
     if (hit_ball.xcor()<-360 and 
-                       hit_ball.xcor()>-370) and 
-                       (hit_ball.ycor()<left_pad.ycor()+40 and 
+                       hit_ball.xcor()>-370 and 
+                       (hit_ball.ycor()<left_pad.ycor()+40) and 
                         hit_ball.ycor()>left_pad.ycor()-40): 
         hit_ball.setx(-360) 
         hit_ball.dx*=-1
+turtle.done()
